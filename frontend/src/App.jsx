c37b296eb21ef0eb46533f52f8d599eb8f67f169
Yassine <bouachrinyassin0@gmail.com>
2025-06-24 01:29:44 +0100

    feat: add RAG context enhancement for weather data analysis

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 /* Pages */
@@ -27,29 +26,3 @@ function App() {
 }
 
 export default App;
-=======
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
-/*Pages */
-import Temperature from "./pages/Temperature"
-import Humidity from './pages/Humidity';
-import Home from './pages/Home';
-
-
-function App() {
-
-  return (
-
-    
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/temperature" element={<Temperature />} />
-        <Route path="/humidity" element={<Humidity />} />
-      </Routes>
-    
-    
-  )
-}
-
-export default App;
->>>>>>> upstream/main
